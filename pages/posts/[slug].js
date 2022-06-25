98867f4fd549c8e66466b18ad2299f61106e4537
BobbyDigital2491 <100319461+BobbyDigital2491@users.noreply.github.com>
2022-06-24 23:16:02 -0400

    Update [slug].js

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -27,7 +27,7 @@ export default function Post({ post, morePosts, preview }) {
             <article className="mb-32">
               <Head>
                 <title>
-                  {post.title} | Carolina Crypto Collab {CMS_NAME}
+                  {post.title} | Carolina Crypto Collab Blog {CMS_NAME}
                 </title>
                 <meta property="og:image" content={post.ogImage.url} />
               </Head>
